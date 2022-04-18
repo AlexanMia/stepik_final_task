142bf48494f3472c3340e72fbfb1b5ae2715d807
Inna <idinna91@mail.ru>
2022-04-18 14:57:53 +0300

    "update conftest"

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
 
 
-    parser.addoption('--language', action='store', default=en,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: ru or en")
 
 
