f1498de4bd82fd89c51ca045517703d732d8fed1
Inna <idinna91@mail.ru>
2022-04-20 14:22:43 +0300

    "assert for basket adding function"

---
+++ pages/product_page.py
@@ -0,0 +1,51 @@
+import time
+
+from .base_page import BasePage
+
+from .locators_product_page import ProductPageLocators
+import time
+
+
+
+class ProductPage(BasePage):
+    def go_to_add_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BUTTON_BASKET)
+        basket_button.click()
+
+    def should_be_button_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_BASKET), "Button 'add to basket' is not presented"
+
+
+
+    def should_be_message_about_adding(self):
+        # Сначала проверяем, что элементы присутствуют на странице
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
+            "Product name is not presented")
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
+            "Message about adding is not presented")
+        # Затем получаем текст элементов для проверки
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        # Проверяем, что название товара присутствует в сообщении о добавлении
+        # Это можно было бы сделать с помощью split() и сравнения строк,
+        # Но не вижу необходимости усложнять код
+        assert product_name in message, "No product name in the message"
+
+    def should_be_message_basket_total(self):
+        # Сначала проверяем, что элементы присутствуют на странице
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
+            "Message basket total is not presented")
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), (
+            "Product price is not presented")
+        # Затем получаем текст элементов для проверки
+        message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        # Проверяем, что цена товара присутствует в сообщении со стоимостью корзины
+        assert product_price in message_basket_total, "No product price in the message"
+
+
+
+
+
+
+
