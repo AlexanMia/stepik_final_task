bed9377241bf4b78f1c1786c31556b3ef81cd738
Inna <idinna91@mail.ru>
2022-04-21 15:08:31 +0300

    "update class + new checking"

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,27 @@
 # base_page.py - тут мы храним методы которые применяются по всему проекту вообще, всё завернуто в класс, чтобы было удобно импортировать
 
-
-from selenium.common.exceptions import NoSuchElementException
+from .locators import BasePageLocators
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException    # в начале файла
 import math
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        #self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        #link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
@@ -26,7 +37,6 @@ class BasePage():
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
-
         alert.send_keys(answer)
         alert.accept()
         try:
@@ -37,4 +47,22 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
- 
+#Можно добавить в BasePage абстрактный метод, который проверяет, что элемент не появляется на странице в течение заданного времени:
+    def is_not_element_present(self, how, what, timeout=20):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+# почему-то не видно ожидания (закрывается сразу, после добавления в корзину
+    def is_disappeared(self, how, what, timeout=20):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
