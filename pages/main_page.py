1fc77c4e0652e9f0ce01c7bcc9bd48989527894b
Inna <idinna91@mail.ru>
2022-04-21 15:10:19 +0300

    "add plug"

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,21 +9,11 @@ from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#registration_link")
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-
-
-        #link = self.browser.find_element_by_css_selector("#login_link")
-        #link.click()
-        #alert = self.browser.switch_to.alert  # Добавив обработку alert в метод go_to_login_page, мы восстановим работоспособность всех тестов, не меняя самих тестов
-        #alert.accept()
-
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"  # Обратите внимание здесь на символ *, он указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)   # заглушка
+        # Конструктор выше с ключевым словом super на самом деле только вызывает конструктор класса предка
+        # и передает ему все те аргументы, которые мы передали в конструктор MainPage.
+        # or pass
 
 
 
