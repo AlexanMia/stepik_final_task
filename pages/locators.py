6ff64724c8f5d6e02b4bbc8da41a90afaa9f530f
Inna <idinna91@mail.ru>
2022-04-21 15:09:36 +0300

    "add new locators from main page"

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,12 @@
 # файл для размещения селекторов, чтобы быстро исправить в одном месте
 # locators.py - тут мы храним локаторы, в виде констант. Локаторы каждой отдельной страницы завёрнуты в класс, чтобы было удобно импортировать
 
-
-
 from selenium.webdriver.common.by import By
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 
 
 class MainPageLocators():
@@ -16,4 +19,12 @@ class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
+class ProductPageLocators():
+    BUTTON_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
 
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner ")
