e6f9b1607880e02b07d6819ac5a349dfcd582644
Inna <idinna91@mail.ru>
2022-04-22 16:45:31 +0300

    "add checking about empty basket from product page"

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
+from .pages.basket_page import BasketPage
+from .pages.main_page import MainPage
+from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 import time
 import pytest
 
 
-#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019" here alert!!!
+#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear" here alert!!!
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
@@ -40,6 +43,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #page.should_be_button_add_to_basket()
 
 def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_add_basket()  # выполняем метод страницы — переходим на страницу логина
@@ -54,7 +58,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_guest_should_see_button_add(browser):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_button_add_to_basket()
@@ -98,5 +102,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     time.sleep(5)
 
+@pytest.mark.regression
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
 
-
+    #page = MainPage(browser, link)
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    time.sleep(5)
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_things_in_basket()
+    basket_page.should_be_note_about_empty_basket()
