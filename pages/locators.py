e61e1a6b55250aa1af335ab0664c4d8913f27ce4
Inna <idinna91@mail.ru>
2022-04-22 16:42:35 +0300

    "add locators dealing with basket page"

--- pages/locators.py
+++ pages/locators.py
@@ -28,3 +28,17 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
 
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner ")
+
+
+class BasketPageLocators():
+    BUTTON_BASKET_page = (By.CSS_SELECTOR, "span.btn-group")
+
+    NOTE_ABOUT_BASKET = (By.XPATH, "//div[@id='content_inner']/p")
+    #NOTE_ABOUT_BASKET = ((By.XPATH, "//p[contains(text(), 'empty')]"))
+    ITEMS_IN_BASKET = (By.CSS_SELECTOR, "div.basket-items")
+
+
+
+
+
+#id content_inner - надпись корзина пуста
