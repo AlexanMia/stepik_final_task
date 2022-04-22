cd1a7bae53f2c3f4d7cb0acafbe5f7e017210c1c
Inna <idinna91@mail.ru>
2022-04-22 16:45:14 +0300

    "add checking about empty basket from main page"

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
+import pytest
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import time
 
 #tests in стиле Page Object
 # test_main_page.py - тут мы выполняем сами тесты
@@ -19,6 +23,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     #link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -27,3 +32,35 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+
+@pytest.mark.smoke
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/ru/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    time.sleep(5)
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_things_in_basket()
+    basket_page.should_be_note_about_empty_basket()
+
+
+
+
+
+
+
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    #page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    #page.open()  # открываем страницу
+    #page.go_to_add_basket()
+    #page.go_to_basket_page()
+    #time.sleep(5)
+
+    #basket_page = BasketPage(browser, browser.current_url)
+
+    #basket_page.should_not_be_things_in_basket()
+    #time.sleep(15)
+    #basket_page.should_be_note_about_empty_basket()
